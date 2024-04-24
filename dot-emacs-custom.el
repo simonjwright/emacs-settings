05f2a21de121a541c72dc47d484c1c582b27401f
Simon Wright <simon@pushface.org>
2024-04-24 11:20:13 +0100

    New settings for Inconsolata.

--- dot-emacs-custom.el
+++ dot-emacs-custom.el
@@ -80,4 +80,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "Inconsolata" :foundry "nil" :slant normal :weight medium :width normal)))))
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Inconsolata")))))
